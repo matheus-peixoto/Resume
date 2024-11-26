922ee1830c84ec5e925ee19496ade255c12482ff
Matheus Silva <matheuspeixoto100@gmail.com>
2024-11-26 19:35:03 -0300

    refact: react listing element keys

--- src/app/sections/mainInfo.tsx
+++ src/app/sections/mainInfo.tsx
@@ -4,84 +4,85 @@ import { LuMail } from "react-icons/lu";
 import { LuPhone } from "react-icons/lu";
 
 export function MainInfo() {
-    return (<section className="flex justify-between items-center">
-        <div>
-            <h1 className="text-2xl font-bold text-black">
-                {RESUME.name}
-            </h1>
-            <p className="max-w-md text-pretty text-sm my-2 print:text-[12px]">
-                {RESUME.shortDescription}
-            </p>
-            <p className="max-w-md items-center text-pretty text-xs mb-2">
-                <a
-                    className="inline-flex gap-x-1.5 align-baseline leading-none hover:underline"
-                    href={RESUME.locationLink}
-                    target="_blank"
-                >
-                    <LuGlobe size={12} />
-                    {RESUME.location}
-                </a>
-            </p>
+    return (
+        <section className="flex justify-between items-center">
+            <div>
+                <h1 className="text-2xl font-bold text-black">
+                    {RESUME.name}
+                </h1>
+                <p className="max-w-md text-pretty text-sm my-2 print:text-[12px]">
+                    {RESUME.shortDescription}
+                </p>
+                <p className="max-w-md items-center text-pretty text-xs mb-2">
+                    <a
+                        className="inline-flex gap-x-1.5 align-baseline leading-none hover:underline"
+                        href={RESUME.locationLink}
+                        target="_blank"
+                    >
+                        <LuGlobe size={12} />
+                        {RESUME.location}
+                    </a>
+                </p>
 
-            <div className="flex gap-x-1 print:hidden">
-                {
-                    RESUME.contact.email ? (
-                        <a href={`mailto:${RESUME.contact.email}`} className="inline-flex justify-center items-center border border-gray-200 w-8 h-8 rounded-md transition duration-200 hover:bg-gray-100">
-                            <LuMail size={16} />
-                        </a>
-                    ) : null
-                }
-                {
-                    RESUME.contact.email ? (
-                        <a href={`tel:${RESUME.contact.phone}`} className="inline-flex justify-center items-center border border-gray-200 w-8 h-8 rounded-md duration-200 hover:bg-gray-100">
-                            <LuPhone size={16} />
-                        </a>
-                    ) : null
-                }
-                {
-                    RESUME.contact.socials.map(
-                        social => (
-                            <a key={social.name} href={social.url} target="_blank" className="inline-flex justify-center items-center border border-gray-200 w-8 h-8 rounded-md duration-200 hover:bg-gray-100">
-                                <social.icon />
+                <div className="flex gap-x-1 print:hidden">
+                    {
+                        RESUME.contact.email ? (
+                            <a href={`mailto:${RESUME.contact.email}`} className="inline-flex justify-center items-center border border-gray-200 w-8 h-8 rounded-md transition duration-200 hover:bg-gray-100">
+                                <LuMail size={16} />
+                            </a>
+                        ) : null
+                    }
+                    {
+                        RESUME.contact.email ? (
+                            <a href={`tel:${RESUME.contact.phone}`} className="inline-flex justify-center items-center border border-gray-200 w-8 h-8 rounded-md duration-200 hover:bg-gray-100">
+                                <LuPhone size={16} />
+                            </a>
+                        ) : null
+                    }
+                    {
+                        RESUME.contact.socials.map(
+                            (social, index) => (
+                                <a key={index} href={social.url} target="_blank" className="inline-flex justify-center items-center border border-gray-200 w-8 h-8 rounded-md duration-200 hover:bg-gray-100">
+                                    <social.icon />
+                                </a>
+                            )
+                        )
+                    }
+                </div>
+
+                <div className="hidden print:flex flex-col gap-x-1">
+                    {
+                        RESUME.contact.email
+                        && (
+                            <a href={`mailto:${RESUME.contact.email}`} className="text-sm font-bold">
+                                {RESUME.contact.email}
                             </a>
                         )
-                    )
-                }
-            </div>
+                    }
 
-            <div className="hidden print:flex flex-col gap-x-1">
-                {
-                    RESUME.contact.email
-                    && (
-                        <a href={`mailto:${RESUME.contact.email}`} className="text-sm font-bold">
-                            {RESUME.contact.email}
-                        </a>
-                    )
-                }
-
-                {
-                    RESUME.contact.email
-                    && (
-                        <a href={`tel:${RESUME.contact.phone}`} className="text-sm font-bold">
-                            {RESUME.contact.phone}
-                        </a>
-                    )
-                }
-
-                {
-                    RESUME.contact.socials.map(
-                        social => (
-                            <a key={social.name} className="text-sm font-bold" href={social.url}>{social.url}</a>
+                    {
+                        RESUME.contact.email
+                        && (
+                            <a href={`tel:${RESUME.contact.phone}`} className="text-sm font-bold">
+                                {RESUME.contact.phone}
+                            </a>
                         )
-                    )
-                }
+                    }
+
+                    {
+                        RESUME.contact.socials.map(
+                            (social, index) => (
+                                <a key={index} className="text-sm font-bold" href={social.url}>{social.url}</a>
+                            )
+                        )
+                    }
+                </div>
+
             </div>
 
-        </div>
-
-        <div className="w-full h-full max-w-28 max-h-28 print:hidden">
-            <img className="rounded-md" src={RESUME.ownerImgUrl} alt={RESUME.name + " photo"} />
-        </div>
-    </section>
+            <div className="w-full h-full max-w-28 max-h-28 print:hidden">
+                <img className="rounded-md" src={RESUME.ownerImgUrl} alt={RESUME.name + " photo"} />
+            </div>
+        </section>
     )
 }
