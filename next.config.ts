d56414a887a7620bb9b59e4496b4ad8ee3e53402
Matheus Silva <matheuspeixoto100@gmail.com>
2024-12-06 13:13:19 -0300

    feat: add next build configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: isProd ? '/cv' : '', // The repository name as the base path
+  assetPrefix: isProd ? '/cv/' : '', // The repository name as the asset prefix
+  trailingSlash: true // Required for proper static export on GitHub Pages
 };
 
 export default nextConfig;
